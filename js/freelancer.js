d5b38889557ff56ef07a13c1b6abe8f3d8338834
rofr <robert@devrex.se>
2015-03-29 23:04:07 +0200

    mer bilder och text

--- js/freelancer.js
+++ js/freelancer.js
@@ -17,13 +17,14 @@ $(function() {
 
 // Floating label headings for the contact form
 $(function() {
-    $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
-    }).on("focus", ".floating-label-form-group", function() {
-        $(this).addClass("floating-label-form-group-with-focus");
-    }).on("blur", ".floating-label-form-group", function() {
-        $(this).removeClass("floating-label-form-group-with-focus");
-    });
+  $("a.mailto").attr("href", function () {
+		var domain = "fsq.se";
+		var self = $(this);
+		var href = "mailto:" + self.attr("data-to") + "@" + domain;
+		var subject = self.attr("data-subject");
+		if (subject) href += "?Subject=" + subject;
+		return href;
+	});
 });
 
 // Highlight the top nav as scrolling occurs
